cd3338241250e66a7c2ed9c4b5c647efae812d9d
FujimotoIkuto <r118157@g.kobe-kosen.ac.jp>
2022-11-13 16:49:08 +0900

    アームのクラス化途中、角度シミュレーション作成中

---
+++ src/class_arm.hpp
@@ -0,0 +1,199 @@
+#ifndef ARM_HPP
+#define ARM_HPP
+
+#include "class_joint.hpp"
+#include <ros/ros.h>
+
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+ 
+#include <std_msgs/Bool.h>
+#include <std_msgs/Int16.h>
+#include <geometry_msgs/Pose.h>
+ 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+ 
+#include <dynamixel_sdk/dynamixel_sdk.h>
+#include <dynamixel_wrapper/dynamixel_wrapper.h>
+ 
+#include <Eigen/Core>
+#include <Eigen/LU>
+#include <Eigen/Dense>
+
+class Arm
+{
+    private:
+        const double deg2rad = M_PI/180.0, rad2deg = 180.0/M_PI;
+        dynamixel_wrapper::dynamixel_wrapper_base _dxl_base;
+
+    public:
+        Arm();
+
+        void initialize();
+        
+        Joint joint_offset, joint0, joint1, joint2, joint3, joint4, joint5;
+        
+        void setAngle(Eigen::Matrix<double, 6, 1> angle_rad);
+        void print();
+        void update();
+        void forwardKinematics();
+        void inverseKinematics();
+        void tf_broadcaster();
+};
+
+Arm::Arm()
+{
+    initialize();
+}
+
+void Arm::initialize()
+{
+    #ifndef SIMULATION
+    std::string _port_name("/dev/ttyUSB0");
+    int _baudrate = 1000000;
+    _dxl_base.initialize(_port_name, _baudrate);
+    #endif
+    joint_offset.initialize(  0.0, 0.0, 159.0, 'o');
+          joint0.initialize(  0.0, 0.0,   0.0, 'z',                                 0.0, 1, _dxl_base, dynamixel_wrapper::PH54_200_S500_R, 4);
+          joint1.initialize( 30.0, 0.0, 264.0, 'y',  atan(30.0/264.0)                  , 2, _dxl_base, dynamixel_wrapper::H54_200_S500_R,  4);
+          joint2.initialize(-30.0, 0.0, 258.0, 'y', -atan(30.0/264.0)+atan(-30.0/258.0), 3, _dxl_base, dynamixel_wrapper::H54_100_S500_R,  4);
+          joint3.initialize(  0.0, 0.0,   0.0, 'z',                                 0.0, 4, _dxl_base, dynamixel_wrapper::H54_100_S500_R,  4);
+          joint4.initialize(  0.0, 0.0, 123.0, 'y',                  -atan(-30.0/258.0), 5, _dxl_base, dynamixel_wrapper::H42_020_S300_R,  4);
+          joint5.initialize(  0.0, 0.0,   0.0, 'z',                                 0.0, 6, _dxl_base, dynamixel_wrapper::H42_020_S300_R,  4);
+    
+}
+
+void Arm::setAngle(Eigen::Matrix<double, 6, 1> angle_rad)
+{
+    joint0.setGoalPosition(angle_rad(0,0));
+    joint1.setGoalPosition(angle_rad(1,0));
+    joint2.setGoalPosition(angle_rad(2,0));
+    joint3.setGoalPosition(angle_rad(3,0));
+    joint4.setGoalPosition(angle_rad(4,0));
+    joint5.setGoalPosition(angle_rad(5,0));
+}
+
+void Arm::print()
+{
+    std::cout << 
+    joint0.simulationAngle() << "  " << 
+    joint1.simulationAngle() << "  " << 
+    joint2.simulationAngle() << "  " << 
+    joint3.simulationAngle() << "  " << 
+    joint4.simulationAngle() << "  " << 
+    joint5.simulationAngle() << std::endl;
+}
+
+void Arm::tf_broadcaster()
+{
+    static tf2_ros::TransformBroadcaster br;
+    static geometry_msgs::TransformStamped transformStamped;
+    static tf2::Quaternion q;
+    
+    // Joint 0
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "arm_base_link";
+    transformStamped.child_frame_id = "joint0";
+    transformStamped.transform.translation.x = joint_offset.simulationLink('x');
+    transformStamped.transform.translation.y = joint_offset.simulationLink('y');
+    transformStamped.transform.translation.z = joint_offset.simulationLink('z');
+    
+    // q.setRPY(joint0.simulationAngle('x'), joint0.simulationAngle('y'), joint0.simulationAngle('z'));
+    q.setRPY(0, 0, joint0.simulationAngle());
+    transformStamped.transform.rotation.x = q.x();
+    transformStamped.transform.rotation.y = q.y();
+    transformStamped.transform.rotation.z = q.z();
+    transformStamped.transform.rotation.w = q.w();
+
+    br.sendTransform(transformStamped);
+    
+    // Joint 1
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "joint0";
+    transformStamped.child_frame_id = "joint1";
+    transformStamped.transform.translation.x = joint0.simulationLink('x');
+    transformStamped.transform.translation.y = joint0.simulationLink('y');
+    transformStamped.transform.translation.z = joint0.simulationLink('z');
+    
+    // q.setRPY(joint1.simulationAngle('x'), joint1.simulationAngle('y'), joint1.simulationAngle('z'));
+    q.setRPY(0,joint1.simulationAngle(), 0);
+    transformStamped.transform.rotation.x = q.x();
+    transformStamped.transform.rotation.y = q.y();
+    transformStamped.transform.rotation.z = q.z();
+    transformStamped.transform.rotation.w = q.w();
+
+    br.sendTransform(transformStamped);
+
+    // Joint 2
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "joint1";
+    transformStamped.child_frame_id = "joint2";
+    transformStamped.transform.translation.x = joint1.simulationLink('x');
+    transformStamped.transform.translation.y = joint1.simulationLink('y');
+    transformStamped.transform.translation.z = joint1.simulationLink('z');
+    
+    // q.setRPY(joint2.simulationAngle('x'), joint2.simulationAngle('y'), joint2.simulationAngle('z'));
+    q.setRPY(0,joint2.simulationAngle(), 0);
+    transformStamped.transform.rotation.x = q.x();
+    transformStamped.transform.rotation.y = q.y();
+    transformStamped.transform.rotation.z = q.z();
+    transformStamped.transform.rotation.w = q.w();
+
+    br.sendTransform(transformStamped);
+
+    // Joint 3
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "joint2";
+    transformStamped.child_frame_id = "joint3";
+    transformStamped.transform.translation.x = joint2.simulationLink('x');
+    transformStamped.transform.translation.y = joint2.simulationLink('y');
+    transformStamped.transform.translation.z = joint2.simulationLink('z');
+    
+    // q.setRPY(joint3.simulationAngle('x'), joint3.simulationAngle('y'), joint3.simulationAngle('z'));
+    q.setRPY(0, 0, joint3.simulationAngle());
+    transformStamped.transform.rotation.x = q.x();
+    transformStamped.transform.rotation.y = q.y();
+    transformStamped.transform.rotation.z = q.z();
+    transformStamped.transform.rotation.w = q.w();
+
+    br.sendTransform(transformStamped);
+
+    // Joint 4
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "joint3";
+    transformStamped.child_frame_id = "joint4";
+    transformStamped.transform.translation.x = joint3.simulationLink('x');
+    transformStamped.transform.translation.y = joint3.simulationLink('y');
+    transformStamped.transform.translation.z = joint3.simulationLink('z');
+    
+    // q.setRPY(joint4.simulationAngle('x'), joint4.simulationAngle('y'), joint4.simulationAngle('z'));
+    q.setRPY(0,joint4.simulationAngle(), 0);
+    transformStamped.transform.rotation.x = q.x();
+    transformStamped.transform.rotation.y = q.y();
+    transformStamped.transform.rotation.z = q.z();
+    transformStamped.transform.rotation.w = q.w();
+
+    br.sendTransform(transformStamped);
+
+    // Joint 5
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "joint4";
+    transformStamped.child_frame_id = "joint5";
+    transformStamped.transform.translation.x = joint4.simulationLink('x');
+    transformStamped.transform.translation.y = joint4.simulationLink('y');
+    transformStamped.transform.translation.z = joint4.simulationLink('z');
+    
+    // q.setRPY(joint5.simulationAngle('x'), joint5.simulationAngle('y'), joint5.simulationAngle('z'));
+    q.setRPY(0, 0, joint5.simulationAngle());
+    transformStamped.transform.rotation.x = q.x();
+    transformStamped.transform.rotation.y = q.y();
+    transformStamped.transform.rotation.z = q.z();
+    transformStamped.transform.rotation.w = q.w();
+
+    br.sendTransform(transformStamped);
+}
+#endif
---
+++ src/class_joint.hpp
@@ -0,0 +1,200 @@
+#ifndef JOINT_HPP
+#define JOINT_HPP
+
+#include <ros/ros.h>
+
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+ 
+#include <std_msgs/Bool.h>
+#include <std_msgs/Int16.h>
+#include <geometry_msgs/Pose.h>
+ 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+ 
+#include <dynamixel_sdk/dynamixel_sdk.h>
+#include <dynamixel_wrapper/dynamixel_wrapper.h>
+ 
+#include <Eigen/Core>
+#include <Eigen/LU>
+#include <Eigen/Dense>
+
+class Joint
+{
+    private:
+        const double deg2rad = M_PI/180.0, rad2deg = 180.0/M_PI, rpm2radps = 2*M_PI/60.0, radps2rpm = 60.0/(2*M_PI);
+        char _axis;
+        Eigen::Matrix<double, 3, 1> _link;
+        dynamixel_wrapper::dynamixel_wrapper _motor;
+        double _homing_offset;
+
+        bool _is_valid, _is_valid_old = true;
+
+        double _sensor_angle, _sensor_angular_velocity;
+        double _simulation_angle, _simulation_angular_velocity;
+        double _motor_angle, _motor_angular_velocity;
+    public:
+        // Initialize Joint
+        Joint();
+        void initialize(double link_x, double link_y, double link_z, char axis);
+        void initialize(double link_x, double link_y, double link_z, char axis, double homing_offset, int id, dynamixel_wrapper::dynamixel_wrapper_base dxl_base, dynamixel_wrapper::dynamixel_wrapper_config motor_config, int operating_mode);
+        void setHomingOffset(double homing_offset);
+        
+        void setTorqueEnable(bool state);
+        void setLED(int red, int green, int blue);
+
+        double getPresentPosition();
+        double getPresentVelocity();
+        void setGoalPosition(double motor_angle_rad);
+        void setGOalVelocity(double motor_angular_velocity_radps);
+
+        void print();
+        void update();
+        Eigen::Matrix<double, 3, 3> getRotationMatrix();
+
+        // Simulation
+        Eigen::Matrix<double, 3, 1> simulationLink();
+        double simulationAngle(char axis);
+        double simulationAngle();
+        double simulationAngularVelocity(char axis);
+};
+
+Joint::Joint(){}
+
+void Joint::initialize(double link_x, double link_y, double link_z, char axis)
+{
+    _axis = axis;
+    _link << link_x, link_y, link_z;
+}
+void Joint::initialize(double link_x, double link_y, double link_z, char axis, double homing_offset, int id, dynamixel_wrapper::dynamixel_wrapper_base dxl_base, dynamixel_wrapper::dynamixel_wrapper_config motor_config, int operating_mode)
+{
+    initialize(link_x, link_y, link_z, axis);
+    #ifndef SIMULATION
+    _motor.initialize(id, dxl_base, motor_config, operating_mode);
+    #endif
+    setTorqueEnable(false);
+    setHomingOffset(homing_offset);
+
+}
+
+void Joint::setTorqueEnable(bool state)
+{
+    #ifndef SIMULATION
+    _motor.setTorqueEnable(state);
+    #endif
+}
+
+void Joint::setHomingOffset(double homing_offset)
+{
+    setTorqueEnable(false);
+    _homing_offset = homing_offset;
+    #ifndef SIMULATION
+    _motor.setHomingOffset(_homing_offset*rad2deg);
+    #endif
+}
+
+void Joint::setLED(int red, int green, int blue)
+{
+    #ifndef SIMULATION
+    _motor.setLED(red, green, blue);
+    #endif
+}
+
+double Joint::getPresentPosition()
+{
+    #ifndef SIMULATION
+    _sensor_angle = _motor.getPresentPosition()*deg2rad;
+    #endif
+    #ifdef SIMULATION
+    _sensor_angle = _simulation_angle;
+    #endif
+    return _sensor_angle;
+}
+
+double Joint::getPresentVelocity()
+{
+    #ifndef SIMULATION
+    _sensor_angular_velocity = _motor.getPresentVelocity()*rpm2radps;
+    #endif
+    #ifdef SIMULATION
+    _sensor_angular_velocity = _simulation_angular_velocity;
+    #endif
+    return _sensor_angular_velocity;
+}
+
+void Joint::setGoalPosition(double motor_angle_rad)
+{
+    if(isnan(motor_angle_rad))
+    {
+        _is_valid = false;
+    }
+    if(_is_valid != _is_valid_old)
+    {
+        setLED(_is_valid?255:0, _is_valid?0:255, 0);
+    }
+    if(_is_valid)
+    {
+        _motor_angle = motor_angle_rad;
+        #ifndef SIMULATION
+        _motor.setGoalPosition(_motor_angle*rad2deg);
+        #endif
+        #ifdef SIMULATION
+        _simulation_angle = _motor_angle;
+        #endif
+    }
+    _is_valid_old = _is_valid_old;
+}
+
+void Joint::setGOalVelocity(double motor_angular_velocity_radps)
+{
+    if(isnan(motor_angular_velocity_radps))
+    {
+        _is_valid = false;
+    }
+    if(_is_valid != _is_valid_old)
+    {
+        setLED(_is_valid?255:0, _is_valid?0:255, 0);
+    }
+    if(_is_valid)
+    {
+        _motor_angular_velocity = motor_angular_velocity_radps;
+        #ifndef SIMULATION
+        _motor.setGoalVelocity(_motor_angular_velocity*radps2rpm);
+        #endif
+        #ifdef SIMULATION
+        _simulation_angular_velocity = _motor_angular_velocity;
+        #endif
+    }
+    _is_valid_old = _is_valid_old;
+}
+
+void Joint::print()
+{
+    std::cout << _axis << std::endl << std::endl;
+    std::cout << _link << std::endl;
+}
+
+// Simulation
+Eigen::Matrix<double, 3, 1> Joint::simulationLink()
+{
+    return _link*10e-3;
+}
+double Joint::simulationAngle(char axis)
+{
+    if(axis == _axis) return _simulation_angle-_homing_offset;
+    else return 0.0;
+}
+double Joint::simulationAngle()
+{
+    return _simulation_angle;//-_homing_offset;
+}
+double Joint::simulationAngularVelocity(char axis)
+{
+    if(axis == _axis) return _simulation_angular_velocity;
+    else return 0.0;
+}
+#endif
